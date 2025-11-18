c775c08c969b9a8de0f7dc322126744ec1f71fe4
MichaelDik <mwdik@comcast.net>
2025-11-18 15:38:02 -0500

    refactored tools to its own module

--- delete/delete.py
+++ delete/delete.py
@@ -1,25 +1,8 @@
 from openai import OpenAI
 import json
+from tools import tools, get_lucky_number # My Tools package 
 
 client = OpenAI()
-
-
-def get_lucky_number():
-    return 42
-
-
-tools = [
-    {
-        "type": "function",
-        "name": "get_lucky_number",
-        "description": "Return a hardcoded lucky number",
-        "parameters": {
-            "type": "object",
-            "properties": {},
-            "required": [],
-        },
-    }
-]
 
 
 # First call: Ask the model to use the tool 
---
+++ delete/tools.py
@@ -0,0 +1,16 @@
+def get_lucky_number():
+    return 42
+
+
+tools = [
+    {
+        "type": "function",
+        "name": "get_lucky_number",
+        "description": "Return a hardcoded lucky number",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": [],
+        },
+    }
+]
